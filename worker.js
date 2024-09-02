fedee608e81bea2f0b59c6bc121834058a8868d5
SeesKo <seeskome@seeskosee.com>
2024-09-01 17:50:55 -0700

    email job processing

--- worker.js
+++ worker.js
@@ -1,12 +1,14 @@
 import Bull from 'bull';
-import sharp from 'sharp';
+import imageThumbnail from 'image-thumbnail';
 import fs from 'fs';
 import { ObjectId } from 'mongodb';
 import dbClient from './utils/db';
 
-// Initialize Bull Queue
+// Initialize Bull Queues
 const fileQueue = new Bull('fileQueue');
+const userQueue = new Bull('userQueue');
 
+// Process the fileQueue for generating thumbnails
 fileQueue.process(async (job) => {
   const { userId, fileId } = job.data;
 
@@ -28,10 +30,31 @@ fileQueue.process(async (job) => {
   const sizes = [500, 250, 100];
   const promises = sizes.map(async (size) => {
     const thumbnailPath = `${fileDocument.localPath}_${size}`;
-    await sharp(fileDocument.localPath).resize(size).toFile(thumbnailPath);
+    const options = { width: size };
+    const thumbnail = await imageThumbnail(fileDocument.localPath, options);
+    fs.writeFileSync(thumbnailPath, thumbnail);
   });
 
   await Promise.all(promises);
 });
 
-export default fileQueue;
+// Process the userQueue for sending welcome email
+userQueue.process(async (job) => {
+  const { userId } = job.data;
+
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+
+  // Fetch user document from DB
+  const userDocument = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+  if (!userDocument) {
+    throw new Error('User not found');
+  }
+
+  // Print welcome message
+  console.log(`Welcome ${userDocument.email}!`);
+});
+
+// Default export with queues as properties
+export default { fileQueue, userQueue };
